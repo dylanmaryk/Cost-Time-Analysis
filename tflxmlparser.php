201289f8c1e3fa7a4be46e96c9afeba61a8ac3eb
tycho <tycho@localhost>
2013-08-05 08:22:23 -0700

    various changes

--- tflxmlparser.php
+++ tflxmlparser.php
@@ -1,8 +1,8 @@
+
 <?php
 
 // Make a web request for a short link to the Wikipedia article
 $curl = curl_init();
-
 $tflurlquery = 'http://journeyplanner.tfl.gov.uk/user/XML_TRIP_REQUEST2?language=en&place_origin=London&type_origin=locator&name_origin=';
 $tflurlquery .= $originpostcode;
 $tflurlquery .= '&place_destination=London&type_destination=locator&name_destination=';
@@ -16,11 +16,12 @@ CURLOPT_URL => $tflurlquery,
 CURLOPT_USERAGENT => 'INSERT_DESCRIPTION_OF_SERVICE, INSERT_EMAIL_ADDRESS'
 ));
 
-$result = curl_exec($curl);
-
-$xmlstring = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-
+$xmlstring = curl_exec($curl);
+//probably should check this
+$responsecode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+echo $xmlstring;
 $xml = simplexml_load_string($xmlstring);
 
 
 ?>
+
