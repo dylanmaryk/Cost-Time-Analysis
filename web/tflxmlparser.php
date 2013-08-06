650c700f0b66491904a174c9fa65bf95ab624b74
Dylan Maryk <dylan@dylanmaryk.com>
2013-08-06 11:57:17 +0100

    Started bringing PHP to web.

--- web/tflxmlparser.php
+++ web/tflxmlparser.php
@@ -28,3 +28,39 @@
 	//var_dump($routes);
 	//echo $routes->asXML();
 ?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Time/Cost Analysis</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+  </head>
+  <body>
+    <div class="container">
+      <h1 align="center">Time/Cost Analysis</h1>
+      <hr>
+      <table class="table">
+        <thead>
+          <th></th>
+          <th><b>Time</b></th>
+          <th><b>Cost</b></th>
+        </thead>
+        <tbody>
+          <tr>
+            <td><b>Bus</b></td>
+            <td>1 hr 15 mins</td>
+            <td>&pound;2.20</td>
+          </tr>
+          <tr>
+            <td><b>Tube</b></td>
+            <td>45 mins</td>
+            <td>&pound;4.30</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <script src="http://code.jquery.com/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/respond.js"></script>
+  </body>
+</html>
