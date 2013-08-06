362daa91394b7f7335d3595f670457f8cb7afc09
Dylan Maryk <dylan@dylanmaryk.com>
2013-08-06 11:34:01 +0100

    Without the loop, works so far.

--- tflxmlparser.php
+++ tflxmlparser.php
@@ -1,36 +1,30 @@
 <?php
+	$originpostcode = 'AL2 1AE';
+	$destinationpostcode = 'SW1H 0BD';
+	$safeorigin = urlencode($originpostcode);
+	$safedestination = urlencode($destinationpostcode);
 
-$originpostcode = 'AL2 1AE';
-$destinationpostcode = 'SW1H 0BD';
-$safeorigin = urlencode($originpostcode);
-$safedestination = urlencode($destinationpostcode);
+	$tflurlquery = 'http://journeyplanner.tfl.gov.uk/user/XML_TRIP_REQUEST2?language=en&place_origin=London&place_destination=London&type_origin=locator&name_origin=';
+	$tflurlquery .= $safeorigin;
+	$tflurlquery .= '&type_destination=locator&name_destination=';
+	$tflurlquery .= $safedestination;
+	// use if we want to allow future calculations
+	//$tflurlquery .= '&itdDate=' .$date. ' &itdTime=' $time;
 
-$tflurlquery = 'http://journeyplanner.tfl.gov.uk/user/XML_TRIP_REQUEST2?language=en&place_origin=London&place_destination=London&type_origin=locator&name_origin=';
-$tflurlquery .= $safeorigin;
-$tflurlquery .= '&type_destination=locator&name_destination=';
-$tflurlquery .= $safedestination;
-// use if we want to allow future calculations
-//$tflurlquery .= '&itdDate=' .$date. ' &itdTime=' $time;
+	$curl = curl_init();
+	curl_setopt_array($curl, array(
+		CURLOPT_RETURNTRANSFER => 1,
+		CURLOPT_URL => $tflurlquery,
+		CURLOPT_USERAGENT => 'INSERT_DESCRIPTION_OF_SERVICE, INSERT_EMAIL_ADDRESS'
+	));
 
-$curl = curl_init();
-curl_setopt_array($curl, array(
-CURLOPT_RETURNTRANSFER => 1,
-CURLOPT_URL => $tflurlquery,
-CURLOPT_USERAGENT => 'INSERT_DESCRIPTION_OF_SERVICE, INSERT_EMAIL_ADDRESS'
-));
-
-$xmlstring = curl_exec($curl);
-//probably should check this
-$responsecode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-//echo $xmlstring;
-$xml = simplexml_load_string($xmlstring);
-//var_dump($xml);
-$routes = $xml->itdTripRequest->itdItinerary->itdRouteList;
-//var_dump($routes);
-//echo $routes->asXML();
-
-// iterate through all the routes and print out the start and end times.
-foreach ($routes as $route) {
-   echo ;
-}
+	$xmlstring = curl_exec($curl);
+	//probably should check this
+	$responsecode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+	//echo $xmlstring;
+	$xml = simplexml_load_string($xmlstring);
+	//var_dump($xml);
+	$routes = $xml->itdTripRequest->itdItinerary->itdRouteList;
+	//var_dump($routes);
+	//echo $routes->asXML();
 ?>
