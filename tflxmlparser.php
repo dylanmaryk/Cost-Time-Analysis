58033818284c644921f0a8f20c170758a87ceb8d
Shaun S <theguywholikeslinux@gmail.com>
2013-08-06 11:13:23 +0100

    unfunctional commit...

--- tflxmlparser.php
+++ tflxmlparser.php
@@ -1,15 +1,18 @@
 <?php
-$originpostcode = 'AL2%201AE';
-$destinationpostcode = 'SW1H%200BD';
-// Make a web request for a short link to the Wikipedia article
-$curl = curl_init();
+
+$originpostcode = 'AL2 1AE';
+$destinationpostcode = 'SW1H 0BD';
+$safeorigin = urlencode($originpostcode);
+$safedestination = urlencode($destinationpostcode);
+
 $tflurlquery = 'http://journeyplanner.tfl.gov.uk/user/XML_TRIP_REQUEST2?language=en&place_origin=London&place_destination=London&type_origin=locator&name_origin=';
-$tflurlquery .= $originpostcode;
+$tflurlquery .= $safeorigin;
 $tflurlquery .= '&type_destination=locator&name_destination=';
-$tflurlquery .= $destinationpostcode;
+$tflurlquery .= $safedestination;
 // use if we want to allow future calculations
 //$tflurlquery .= '&itdDate=' .$date. ' &itdTime=' $time;
 
+$curl = curl_init();
 curl_setopt_array($curl, array(
 CURLOPT_RETURNTRANSFER => 1,
 CURLOPT_URL => $tflurlquery,
@@ -23,6 +26,11 @@ $responsecode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
 $xml = simplexml_load_string($xmlstring);
 //var_dump($xml);
 $routes = $xml->itdTripRequest->itdItinerary->itdRouteList;
-var_dump($routes);
-echo $routes->asXML();
+//var_dump($routes);
+//echo $routes->asXML();
+
+// iterate through all the routes and print out the start and end times.
+foreach ($routes as $route) {
+   echo ;
+}
 ?>
