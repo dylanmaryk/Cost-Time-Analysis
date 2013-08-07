c3e17fa794e8e9cbf609060fc329ed73261d3ee4
Shaun S <theguywholikeslinux@gmail.com>
2013-08-07 12:47:49 +0100

    added cost to route class

--- web/route.class.php
+++ web/route.class.php
@@ -8,13 +8,15 @@ class route
 	public $detailsLink; // string
 	public $interchanges; // transportType[]. That is to say an array of
 	                      // transportType objects.
+	public $cost;
 	
-	public function route($de, $ar, $du, $li, $in) {
+	public function route($de, $ar, $du, $li, $in, $co) {
 		$this->departure = $de;
 		$this->arrival = $ar;
 		$this->duration = $du;
 		$this->detailsLink = $li;
 		$this->interchanges = $in;
+		$this->cost = $co;
 	}
 }
 
