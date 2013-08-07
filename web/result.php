1a24046820b8fb8e82c6b8e10676f7cd220af0df
Dylan Maryk <dylan@dylanmaryk.com>
2013-08-07 14:40:14 +0100

    Don't mess with my files!

--- web/result.php
+++ web/result.php
@@ -7,16 +7,8 @@
 <html lang='en'>
   <head>
     <title>Time/Cost Analysis</title>
-<<<<<<< HEAD
-    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-    <link href='css/bootstrap.min.css' rel='stylesheet' media='screen'>
-=======
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
-    <style type="text/css">
-      
-    </style>
->>>>>>> master
   </head>
   <body style='padding-top: 20px;'>
     <div class='container'>
