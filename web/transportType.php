57501ca3e6547ee636b63d88c8e14f33df68d64f
Shaun S <theguywholikeslinux@gmail.com>
2013-08-07 11:53:14 +0100

    Changes to transportType class

--- web/transportType.php
+++ web/transportType.php
@@ -1,11 +1,29 @@
 <?php
 
-class transportType
+/**
+ * Stores information about each type of transport.
+ */
+abstract class transportType
 {
-	public $englishName = "";
-	public $imgURI = "";
-	public function price($subTotal) {
+	// ID is the German name found in the productName attribute of
+	//the subroute.
+	public static $ID = '';
+	public static $englishName = '';
+	public static $imgURI = '';
+	
+	public abstract function price($subTotal, $start, $end) {
 		// work out the price of this subRoute
+	}
+}
+
+class bus extends transportType
+{
+	public static $ID = 'Bus';
+	public static $englishName = 'Bus';
+	public static $imgURI = '/user/assets/images/icon-buses.gif';
+	
+	public function price($subTotal, $start, $end) {
+		
 	}
 }
 
