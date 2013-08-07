7b3d9f29b782110e0b24726530f99deeb109192d
Shaun S <theguywholikeslinux@gmail.com>
2013-08-07 11:57:07 +0100

    added walking

--- web/transportType.php
+++ web/transportType.php
@@ -10,6 +10,7 @@ abstract class transportType
 	public static $ID = '';
 	public static $englishName = '';
 	public static $imgURI = '';
+	public static $imgDomain = 'http://journeyplanner.tfl.gov.uk';
 	
 	public abstract function price($subTotal, $start, $end) {
 		// work out the price of this subRoute
@@ -23,7 +24,18 @@ class bus extends transportType
 	public static $imgURI = '/user/assets/images/icon-buses.gif';
 	
 	public function price($subTotal, $start, $end) {
-		
+		// bus specific price calculations
+	}
+}
+
+class walk extends transportType
+{
+	public static $ID = 'Fussweg';
+	public static $englishName = 'Walk';
+	public static $imgURI = '/user/assets/images/icon-walk.gif';
+	
+	public function price($subTotal, $start, $end) {
+		return 0;
 	}
 }
 
