445435cacb18aed46b1595492b2caf20000009ef
Shaun S <theguywholikeslinux@gmail.com>
2013-08-08 11:49:31 +0100

    changed indentation

--- web/result.php
+++ web/result.php
@@ -19,46 +19,46 @@
       <img src='img/logo.png' style='width: 50%; height: 50%; margin-left: auto; margin-right: auto; display: block;' />
       <hr>
       <?php if(!$invalidPostcode) { ?>
-      <table class='table' style='width: 360px; margin: auto;'>
-        <thead>
-          <th></th>
-          <th><b>Start</b></th>
-          <th><b>End</b></th>
-          <th><b>Duration</b></th>
-          <th><b>Cost</b></th>
-          <th></th>
-        </thead>
-        <tbody>
-          <?php
-            $i = 1;
-
-            foreach ($routes as $routeElement) {
-              ?>
-              <tr>
-                <td><b><?php echo $i ?></b></td>
-                <td><?php echo $routeElement->departure ?></td>
-                <td><?php echo $routeElement->arrival   ?></td>
-                <td><?php echo $routeElement->duration  ?></td>
-                <td>&pound;<?php printf("%01.2f", $routeElement->cost/100)?></td>
-                <td><a href="<?php echo $routeElement->detailsLink ?>">Details</a></td>
-              </tr>
-              <tr>
-                <td colspan="6" style="border-top: none;">
-                  <b>Interchanges:</b>
-                  <div style="float: right;">
-                    <?php foreach ($routeElement->interchanges as $interchange) {
-                      echo '<img src="' . transportType::$imgDomain
-                      . $interchange->imgURI . '" alt="'
-                      . $interchange->englishName . '" />';
-                    } ?>
-                  </td>
-                </div>
-              </tr>
-              <?php $i++;
-            }
-          ?>
-        </tbody>
-      </table>
+        <table class='table' style='width: 360px; margin: auto;'>
+          <thead>
+            <th></th>
+            <th><b>Start</b></th>
+            <th><b>End</b></th>
+            <th><b>Duration</b></th>
+            <th><b>Cost</b></th>
+            <th></th>
+          </thead>
+          <tbody>
+            <?php
+              $i = 1;
+  
+              foreach ($routes as $routeElement) {
+                ?>
+                <tr>
+                  <td><b><?php echo $i ?></b></td>
+                  <td><?php echo $routeElement->departure ?></td>
+                  <td><?php echo $routeElement->arrival   ?></td>
+                  <td><?php echo $routeElement->duration  ?></td>
+                  <td>&pound;<?php printf("%01.2f", $routeElement->cost/100)?></td>
+                  <td><a href="<?php echo $routeElement->detailsLink ?>">Details</a></td>
+                </tr>
+                <tr>
+                  <td colspan="6" style="border-top: none;">
+                    <b>Interchanges:</b>
+                    <div style="float: right;">
+                      <?php foreach ($routeElement->interchanges as $interchange) {
+                        echo '<img src="' . transportType::$imgDomain
+                        . $interchange->imgURI . '" alt="'
+                        . $interchange->englishName . '" />';
+                      } ?>
+                    </td>
+                  </div>
+                </tr>
+                <?php $i++;
+              }
+            ?>
+          </tbody>
+        </table>
       <?php } else { ?>
       <h3> Invalid Postcode </h3>
       <?php } ?>
